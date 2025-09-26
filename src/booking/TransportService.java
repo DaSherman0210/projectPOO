d51a8122771c3f7cd64c07b8aa8e4d087b967367
CENTIC <CENTIC@10.6.65.117>
2025-09-26 11:36:01 -0500

    TransportService Commit

--- src/booking/TransportService.java
+++ src/booking/TransportService.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package booking;
 
-/**
- *
- * @author CENTIC
- */
 public class TransportService {
+    private int id;
+    private String origin;
+    private String destination;
+    private double price;
+    
+    public TransportService(){
+    }
+    
+    public TransportService(int id,String origin,String destination,double price){
+        this.setId(id);
+        this.setDestination(destination);
+        this.setOrigin(origin);
+        this.setPrice(price);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "TransportService{" + "id=" + id + ", origin=" + origin + ", destination=" + destination + ", price=" + price + '}';
+    }
     
 }
