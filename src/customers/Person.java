c62ebe8eed0c2639a8b22e64e7e61148e773489f
CENTIC <CENTIC@10.6.65.169>
2025-09-26 11:38:43 -0500

    :feat: class Person done.

--- src/customers/Person.java
+++ src/customers/Person.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package customers;
 
-/**
- *
- * @author CENTIC
- */
 public class Person {
     
+    private String name;
+    private int age;
+    
+    public Person(){
+        this.name = "";
+        this.age = 0;
+    }
+    
+    public Person(String name, int age){
+        this.setName(name);
+        this.setAge(age);
+    }
+    
+    // SETTERS
+    
+    protected final void setName(String name){
+        this.name = name;
+    }
+    
+    protected final void setAge(int age){
+        this.age = age;
+    }
+    
+    // GETTERS
+    
+    protected String getName(){
+        return this.name;
+    }
+    
+    protected int getAge(){
+        return this.age;
+    }
+    
+    @Override
+    
+    public String toString(){
+        return "Name: " + this.getName() + "\n Age: " + this.getAge();
+    }
 }
