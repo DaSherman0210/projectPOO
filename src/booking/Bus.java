9f45ff181669f84fe9270e632d212137c808b000
CENTIC <CENTIC@10.6.65.117>
2025-09-26 11:45:30 -0500

    Bus Commit

--- src/booking/Bus.java
+++ src/booking/Bus.java
@@ -8,6 +8,38 @@ package booking;
  *
  * @author CENTIC
  */
-public class Bus {
+public class Bus extends TransportService{
+    private String busCompany;
+    private String boardingPoint;
+
+    public Bus(){
+        
+    }
+    public Bus(int id,String origin,String destination,double price,String busCompany,String boardingPoint){
+        this.setId(id);
+        this.setOrigin(origin);
+        this.setDestination(destination);
+        this.setPrice(price);
+        this.setBusCompany(busCompany);
+        this.setBoardingPoint(boardingPoint);
+    }
+    
+    
+    public String getBusCompany() {
+        return busCompany;
+    }
+
+    public String getBoardingPoint() {
+        return boardingPoint;
+    }
+
+    public final void setBusCompany(String busCompany) {
+        this.busCompany = busCompany;
+    }
+
+    public final void setBoardingPoint(String boardingPoint) {
+        this.boardingPoint = boardingPoint;
+    }
+    
     
 }
