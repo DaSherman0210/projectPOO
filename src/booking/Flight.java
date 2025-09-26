989098e7b65e72280189d56ab3baca65f685472e
CENTIC <CENTIC@10.6.65.21>
2025-09-26 12:10:45 -0500

    flight commit

--- src/booking/Flight.java
+++ src/booking/Flight.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package booking;
 
-/**
- *
- * @author CENTIC
- */
-public class Flight {
+public class Flight extends TransportService{
+    private String airline;
+    private String seatClass;
+    
+    public Flight(){
+        
+    }
+    
+    public Flight(int id, String origin, String destination, double price, String airline, String seatClass){
+        this.setAirline(airline);
+        this.setSeatClass(seatClass);
+    }
+    
+    public final void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public final void setSeatClass(String seatClass) {
+        this.seatClass = seatClass;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public String getSeatClass() {
+        return seatClass;
+    }
+    
+    
+    @Override
+    public String getServiceInformation() {
+        return this.airline;
+    }
+    
+    
+    @Override
+    public String getLuggageInformation() {
+        return this.seatClass;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight" + "\nairline=" + airline + ", \nseatClass=" + seatClass;
+    }
+    
     
 }
