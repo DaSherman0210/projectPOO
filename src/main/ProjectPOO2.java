334f74fc9812e5dd581ff98d655bf4f900d747a8
CENTIC <CENTIC@10.6.65.117>
2025-09-30 11:49:16 -0500

    :feat: cruiser implementation

--- src/main/ProjectPOO2.java
+++ src/main/ProjectPOO2.java
@@ -13,12 +13,16 @@ public class ProjectPOO2 {
             
             Passenger ps1 = new Passenger("Oscar", 23, "A567S123");
             Passenger ps2 = new Passenger("Joseph", 18, "FHJG53221V");
+            Passenger ps3=new Passenger("Luis",25,"A92K3OE20");
             
             TransportService fligth1 = new Flight();
             TransportService bus1 = new Bus();
+            TransportService cruiser1=new Cruiser();
             
             ps1.addTransportService(bus1);
             ps1.addTransportService(fligth1);
+            ps2.addTransportService(cruiser1);
+            ps3.addTransportService(bus1);
             
             //ps1.addTransportService(12, "Sao Paulo", "Giron", 23000000);
             //ps2.addTransportService(13, "Cambagdu", "La guajira", 123300000);
@@ -26,6 +30,7 @@ public class ProjectPOO2 {
             System.out.println("-------------PASSENGERS-------------");
             System.out.println(ps1);
             System.out.println(ps2);
+            System.out.println(ps3);
             System.out.println("-------------------------------------");
             
         }catch(Exception e){
